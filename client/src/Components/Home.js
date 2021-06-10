7ddd12464e289c33afc78e78550204737ad3dbd6
Durgesh Nandan <dnandan@synamedia.com>
2021-06-10 15:28:30 +0530

    Live Home

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,5 +1,9 @@
 import React, { Component,useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { COLOR } from "../Constants/Color";
+import { Row, Col, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import { Paper } from "@material-ui/core";
+
 
 
 const Home = (props) => 
@@ -27,21 +31,35 @@ const Home = (props) =>
     },[])
 
     return (
-        <div className="container">
-            <div className="row">
-                {videos.map(video =>
-                <div className="col-md-4" key={video.id}>
-                    <Link to={`/player/${video.id}`}>
-                        <div className="card border-0">
-                            <img src={`http://localhost:4000${video.poster}`} alt={video.name} />
-                            <div className="card-body">
-                                <p>{video.name}</p>
-                            </div>
-                        </div>
-                    </Link>
-                </div>
-                )}
-            </div>
+        <div >
+            <Paper elevation={3} variant="outlined" className="top_bar text-white mt-4 mb-3 ml-4 mr-4  p-3">
+				<Row>
+					<Col md={5} className="summary_heading">
+                        Live Videos
+					</Col>
+				</Row>
+				
+            </Paper>
+            <Paper elevation={3} variant="outlined" className="bg-dark-local text-white mt-2 ml-4 mr-4" style={{backgroundColor: COLOR.COMPONENT_BACKGROUND_BLUE}}>
+                <Row style={{marginLeft:'1px',marginRight:'1px',padding:'50px'}}>
+                        {videos.map(video =>
+                        <Col md={4} key={video.id}>
+                            <Card style={{backgroundColor:COLOR.SELECTED_ROW}}>
+                                <CardImg top width="100%" src={`http://localhost:4000${video.poster}`} alt={video.name} />
+                                <CardBody>
+                                    <CardTitle tag="h5">{video.name}</CardTitle>
+                                    <CardSubtitle tag="h6" className="mb-2 text-muted"><i className="spinner-grow spinner-grow-sm text-danger" /> Live</CardSubtitle>
+                                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                    <Link to={`/player/${video.id}`}>
+                                        <Button>View</Button>
+                                    </Link>
+                                </CardBody>
+                             </Card>
+                        </Col>
+                    )}            
+                </Row>
+            </Paper>
+            
         </div>
     )
 }
--- client/src/Components/Player.js
+++ client/src/Components/Player.js
@@ -1,13 +1,17 @@
 import React, { Component,useState,useEffect } from 'react'
+import { Row, Col } from 'reactstrap'
+import { Paper } from '@material-ui/core';
+import { COLOR } from "../Constants/Color";
 
 const Player = (props) => 
 {
     const [state,setState] = useState({
         videoId : props.match.params.id,
-        videoData : {}
+        videoData : {},
+        seekDuration : 0
     })
 
-    const {videoId,videoData} = state
+    const {videoId,videoData,seekDuration} = state
 
    useEffect(() => 
     {
@@ -15,8 +19,9 @@ const Player = (props) =>
         {
             fetch(`http://localhost:4000/video/${videoId}/data`)
             .then(res => {
-                const data = res.json();
-                setState({ ...state,videoData: data });
+                res.json()
+                .then(data => setState({ ...state,videoData: data }))
+                .catch(e => console.log(e))
             })
             .catch(err =>console.log(err))
         }
@@ -27,12 +32,27 @@ const Player = (props) =>
     },[])
 
     return (    
-        <header className="App-header">
-            <video controls muted autoPlay>
-                <source src={`http://localhost:4000/video/${videoId}`} type="video/mp4"></source>
-            </video>
-            <h1>{ videoData.name }</h1>
-        </header>
+        <div >
+            <Paper elevation={3} variant="outlined" className="top_bar text-white mt-4 mb-3 ml-4 mr-4  p-3">
+				<Row>
+					<Col md={5} className="summary_heading">
+                        { videoData.name }
+					</Col>
+				</Row>
+				
+            </Paper>
+            <Paper elevation={3} variant="outlined" className="bg-dark-local text-white mt-2 ml-4 mr-4" style={{border: `solid 2px ${COLOR.SELECTED_ROW}`, backgroundColor: COLOR.SELECTED_ROW}}>
+                <Row style={{marginLeft:'1px',marginRight:'1px',padding:'50px'}}>
+                    <Col md={5} >
+                        <div style={{width:'89vh', border:'2px solid',borderRadius: '8px'}}>
+                            <video controls muted autoPlay>
+                                <source src={`http://localhost:4000/video/${videoId}`} type="video/mp4"></source>
+                            </video>
+                        </div>
+                    </Col>
+                </Row>
+            </Paper>
+        </div>
     )
 }
 
